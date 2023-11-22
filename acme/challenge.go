ee2a2e07a32d2ee7d8bf3dbe66d0cf796cf31824
beltram <beltram.maldant@gmail.com>
2023-11-22 14:59:45 +0100

    feat: update the protocol by including team & handle in the client dpop token, verifying the handle in the dpop challenge

--- acme/challenge.go
+++ acme/challenge.go
@@ -512,6 +512,8 @@ func wireDPOP01Validate(ctx context.Context, ch *Challenge, db DB, jwk *jose.JSO
 		"verify-access",
 		"--client-id",
 		challengeValues.ClientID,
+		"--handle",
+		challengeValues.Handle,
 		"--challenge",
 		ch.Token,
 		"--leeway",
